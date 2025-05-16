ad8808e2593b05fa9fa08cce8f9b8d2ae3784ad3
r4gend <168578157+r4gend@users.noreply.github.com>
2025-05-16 16:41:24 +0500

    test_items.py

--- test_items.py
+++ test_items.py
@@ -1,10 +1,11 @@
 import time
+from selenium.webdriver.common.by import By  # Добавьте импорт
 
 def test_add_to_basket_button_is_present(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
-    time.sleep(30)  # Для визуальной проверки языка
-    
-    # Проверяем наличие кнопки добавления в корзину
-    add_to_basket_button = browser.find_elements_by_css_selector(".btn-add-to-basket")
-    assert len(add_to_basket_button) > 0, "Add to basket button is not present"
+    time.sleep(5)
+
+    # Новый синтаксис Selenium 4
+    add_to_basket_button = browser.find_elements(By.CSS_SELECTOR, ".btn-add-to-basket")
+    assert len(add_to_basket_button) > 0, "Кнопка добавления в корзину не найдена!"
