e6066fba718ad079365714f25ef48d1ec092cefe
r4gend <168578157+r4gend@users.noreply.github.com>
2025-05-16 16:42:15 +0500

    test_items.py

--- test_items.py
+++ test_items.py
@@ -1,11 +1,10 @@
 import time
-from selenium.webdriver.common.by import By  # Добавьте импорт
+from selenium.webdriver.common.by import By
 
 def test_add_to_basket_button_is_present(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
     time.sleep(5)
 
-    # Новый синтаксис Selenium 4
     add_to_basket_button = browser.find_elements(By.CSS_SELECTOR, ".btn-add-to-basket")
     assert len(add_to_basket_button) > 0, "Кнопка добавления в корзину не найдена!"
